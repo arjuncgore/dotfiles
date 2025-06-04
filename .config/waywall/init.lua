e99a585fd799a8f772465fac38c5c993340fd0f9
Arjun Gore <arjun.c.gore@gmail.com>
2025-06-04 17:52:14 -0400

    Waywall: added alt resolutions

--- .config/waywall/init.lua
+++ .config/waywall/init.lua
@@ -22,7 +22,35 @@ local config = {
     },
 }
 
-config.actions = {}
+local make_res = function(width, height, enable, disable)
+	return function()
+		local active_width, active_height = waywall.active_res()
+
+		if active_width == width and active_height == height then
+			waywall.set_resolution(0, 0)
+			disable()
+		else
+			waywall.set_resolution(width, height)
+			enable()
+		end
+	end
+end
+
+
+
+local resolutions = {
+	thin = make_res(350, 1100, thin_enable, generic_disable),
+	tall = make_res(320, 16384, tall_enable, tall_disable),
+	wide = make_res(2560, 400, wide_enable, generic_disable),
+}
+
+
+config.actions = {
+    ["J"] = resolutions.thin,
+    ["K"] = resolutions.wide,
+    ["L"] = resolutions.tall,
+
+}
 
 return config
 
