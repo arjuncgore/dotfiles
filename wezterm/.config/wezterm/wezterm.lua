61ef320cc47b7a4a2b7c52f743a0558dfd76176c
Arjun Gore <agore1@stevens.edu>
2025-09-16 20:56:23 -0400

    new wezterm config

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,27 @@
+local wezterm = require("wezterm")
+local config = wezterm.config_builder()
+
+config.initial_cols = 120
+config.initial_rows = 58
+
+config.font_size = 12
+config.font = wezterm.font("MesloLGS Nerd Font")
+
+config.color_scheme = "catppuccin-mocha"
+config.window_padding = { left = 20, right = 20, top = 20, bottom = 20 }
+
+config.default_cursor_style = "BlinkingBar"
+config.cursor_blink_ease_in = "Constant"
+config.cursor_blink_ease_out = "Constant"
+config.cursor_blink_rate = 500
+
+config.hide_mouse_cursor_when_typing = true
+config.window_decorations = "RESIZE"
+config.hide_tab_bar_if_only_one_tab = true
+
+config.window_background_opacity = 0.8
+config.macos_window_background_blur = 26
+
+config.automatically_reload_config = true
+
+return config
