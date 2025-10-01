54ee0ca8628338680572b9465be296a8b08394fd
Arjun Gore <arjun.c.gore@gmail.com>
2025-09-30 22:59:43 -0400

    removed fedora sway config

--- swayfed/.config/sway/exit-menu.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-choice=$(echo -e "Exit\nCancel" | wofi --dmenu --width=200 --height=100 --prompt "Exit Sway?" --hide-scroll)
-
-if [ "$choice" = "Exit" ]; then
-    swaymsg exit
-fi
-
--- swayfed/.config/sway/shudown-menu.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-choice=$(echo -e "Shutdown\nCancel" | wofi --dmenu --width=200 --height=100 --prompt "Shutdown?" --hide-scroll)
-
-if [ "$choice" = "Shutdown" ]; then
-    shutdown now
-fi
-
