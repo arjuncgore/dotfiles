e55abec5214149aba5fa356eaf8ac003ad9d4b1f
Arjun Gore <arjun.c.gore@gmail.com>
2025-10-28 17:10:01 -0400

    added more file numbers to harpoon

--- nvim/.config/nvim/lua/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -40,5 +40,8 @@ return {
         vim.keymap.set("n", "<leader>h2", function() harpoon:list():select(2) end, { desc = 'Harpoon: file 2' })
         vim.keymap.set("n", "<leader>h3", function() harpoon:list():select(3) end, { desc = 'Harpoon: file 3' })
         vim.keymap.set("n", "<leader>h4", function() harpoon:list():select(4) end, { desc = 'Harpoon: file 4' })
+        vim.keymap.set("n", "<leader>h5", function() harpoon:list():select(5) end, { desc = 'Harpoon: file 5' })
+        vim.keymap.set("n", "<leader>h6", function() harpoon:list():select(6) end, { desc = 'Harpoon: file 6' })
+        vim.keymap.set("n", "<leader>h7", function() harpoon:list():select(7) end, { desc = 'Harpoon: file 7' })
     end
 }
