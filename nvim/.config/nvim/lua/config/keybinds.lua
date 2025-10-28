16bc156aa121e0ab55c0c2a84f3eb20f78235f77
Arjun Gore <agore1@stevens.edu>
2025-10-28 14:38:48 -0400

    added run keybinds

--- nvim/.config/nvim/lua/config/keybinds.lua
+++ nvim/.config/nvim/lua/config/keybinds.lua
@@ -1,20 +1,24 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>e", vim.cmd.Oil, { desc = "Oil" } )
+vim.keymap.set("n", "<leader>e", vim.cmd.Oil, { desc = "Oil" })
 
 vim.keymap.set({ "n", "t" }, "<leader>t", function()
-  local term_buf = nil
-  for _, buf in ipairs(vim.api.nvim_list_bufs()) do
-    if vim.bo[buf].buftype == "terminal" then
-      term_buf = buf
-      break
+    local term_buf = nil
+    for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+        if vim.bo[buf].buftype == "terminal" then
+            term_buf = buf
+            break
+        end
     end
-  end
 
-  if term_buf and vim.api.nvim_buf_is_valid(term_buf) then
-    vim.cmd("bd! " .. term_buf)         -- close terminal if open
-  else
-    vim.cmd("terminal")                 -- open terminal
-    vim.cmd("startinsert")              -- jump straight into insert (terminal) mode
-  end
+    if term_buf and vim.api.nvim_buf_is_valid(term_buf) then
+        vim.cmd("bd! " .. term_buf) -- close terminal if open
+    else
+        vim.cmd("terminal")         -- open terminal
+        vim.cmd("startinsert")      -- jump straight into insert (terminal) mode
+    end
 end, { desc = "Toggle Terminal" })
 
+-- ==== Run Files ====
+vim.keymap.set("n", "<leader>cl", function()
+    vim.cmd("!pdflatex *.tex")
+end, { desc = "Run: LaTeX Compile" })
