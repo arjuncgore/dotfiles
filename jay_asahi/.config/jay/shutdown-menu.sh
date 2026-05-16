7136b4599618a45621139e35bc5d5df97032fbe9
Gore <arjun.c.gore@gmail.com>
2026-05-16 11:54:38 -0700

    began jay config on asahi

---
+++ jay_asahi/.config/jay/shutdown-menu.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+choice=$(echo -e "Shutdown\nCancel" | wofi --dmenu --width=100 --height=140 --prompt "Shutdown?" --hide-scroll)
+
+if [ "$choice" = "Shutdown" ]; then
+    shutdown now
+fi
+
